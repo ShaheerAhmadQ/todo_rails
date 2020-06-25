c6a4e43ddf56a90e507cc2d20fefaf52d9f7d553
Shaheer Ahmad <saqureshi2809@gmail.com>
2020-06-26 00:57:30 +0500

    Edit functionality working fine now

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,25 +1,32 @@
 class TasksController < ApplicationController
 
-    before_action :temp_obj, only: [:create]
+    #before_action :temp_obj, only: [:create]
 
     def index
         @tasks = Task.where(is_deleted: false).all()
+        p "All tasks #{@task.inspect}"
     end
 
     def show
         @task = Task.where(is_deleted: false).find(params[:id])
+        p "Showing task #{@task.inspect}"
+    end
+
+    def update
+        @task = Task.where(is_deleted: false).find(params[:id])
+        p "Editting task #{@task.inspect}"
+        if @task.update(title: params[:task][:title], body:params[:task][:body], status: params[:task][:status])
+            p "responding with Updated Task : #{@task.inspect}"
+            redirect_to @task
+        else
+            p "responding with Error Updating Task : #{@task.errors.full_messages}"
+            render edit_task_path(@task)
+        end
     end
 
     def edit
         @task = Task.where(is_deleted: false).find(params[:id])
-        render plain: @task.inspect
-        #if @task.update(title: params[:task][:title], body:params[:task][:body], status: params[:task][:status])
-        #    p "responding with Updated Task : #{@task.inspect}"
-        #    redirect_to @task
-        #else
-        #    p "responding with Error Updating Task : #{@task.errors.full_messages}"
-        #    render 'edit'
-        #end
+        p "Editting task #{@task.inspect}"
     end
 
     def create
@@ -35,11 +42,13 @@ class TasksController < ApplicationController
     end
 
     def destroy
-        @task = Task.where(id: params[:id])
+        @task = Task.where(id: params[:id], is_deleted: false)
         if @task.blank?
+            p "No Such undeleted task found for deletion"
             render plain: "No Such record exists"
         else
             @task.update(is_deleted: true)
+            p "Task found and Deleted : #{@task.inspect}"
             redirect_to action: 'index'
         end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   root 'tasks#index'
 
   resources :tasks
-
 end
